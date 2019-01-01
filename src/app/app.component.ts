c658f2bf01d3d5e65e481beb6221d018f0b37da3
binayak-shankar <binayak.shankar@rapidops.com>
2019-01-01 17:29:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-project';
+name: string;
+  constructor(private dataService: DataService) {}
+
+  getName() {
+    this.name = this.dataService.showName();
+  }
+
+  ngOnInit(){
+  this.getName();
+  }
 }
